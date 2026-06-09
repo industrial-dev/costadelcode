769caecfdd07884dd0dfb2d59b99ccc90cfdc2a2
industrialdev <danigevorkian@gmail.com>
2026-06-09 22:18:45 +0200

    ✨feat(home): added Cobe Globe and react integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://industrial-dev.github.io',
   base: process.env.NODE_ENV === 'production' ? '/costadelcode' : '/',
+  integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
   },
